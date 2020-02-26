daff67c8e80fa945ab2cd8172e92c57a25f774a2
Luke Beatty <lcharlesb@Lukes-MacBook-Pro-3.local>
2020-02-25 21:00:57 -0500

    Function outlines for unit tests.

--- unittests.py
+++ unittests.py
@@ -25,5 +25,126 @@ class TestMountain(unittest.TestCase):
         # called after every test
         print('tearDown()')
 
+    # Functions for testing mountain.py
+    def test_mountain_to_string(self):
+        print('TODO')
+
+    def test_mountain_get_name(self):
+        print('TODO')
+
+    def test_mountain_get_location(self):
+        print('TODO')
+
+    def test_mountain_get_trails(self):
+        print('TODO')
+
+    def test_mountain_get_lifts(self):
+        print('TODO')
+
+    def test_mountain_get_lodges(self):
+        print('TODO')
+
+    def test_mountain_trail_status(self):
+        print('TODO')
+
+    def test_mountain_lift_status(self):
+        print('TODO')
+
+    def test_mountain_lodge_status(self):
+        print('TODO')
+
+    def test_mountain_add_trail(self):
+        print('TODO')
+
+    def test_mountain_remove_trail(self):
+        print('TODO')
+
+    def test_mountain_add_lift(self):
+        print('TODO')
+
+    def test_mountain_remove_lift(self):
+        print('TODO')
+
+    def test_mountain_add_lodge(self):
+        print('TODO')
+
+    def test_mountain_remove_lodge(self):
+        print('TODO')
+
+    # Functions for testing trail.py
+    def test_trail_to_string(self):
+        print('TODO')
+
+    def test_trail_get_name(self):
+        print('TODO')
+
+    def test_trail_get_difficulty(self):
+        print('TODO')
+
+    def test_trail_get_groomed(self):
+        print('TODO')
+
+    def test_trail_get_lifts(self):
+        print('TODO')
+
+    def test_trail_get_trail(self):
+        print('TODO')
+
+    def test_trail_add_lift(self):
+        print('TODO')
+
+    def test_trail_remove_lift(self):
+        print('TODO')
+
+    # Functions for testing lift.py
+    def test_lift_to_string(self):
+        print('TODO')
+
+    def test_lift_get_name(self):
+        print('TODO')
+
+    def test_lift_get_running(self):
+        print('TODO')
+
+    def test_lift_get_trails(self):
+        print('TODO')
+
+    def test_lift_run_lift(self):
+        print('TODO')
+
+    def test_lift_stop_lift(self):
+        print('TODO')
+
+    def test_lift_add_trail(self):
+        print('TODO')
+
+    def test_lift_remove_trail(self):
+        print('TODO')
+
+    # Functions for testing lodge.py
+    def test_lodge_to_string(self):
+        print('TODO')
+
+    def test_lodge_get_name(self):
+        print('TODO')
+
+    def test_lodge_get_open(self):
+        print('TODO')
+
+    def test_lodge_get_food(self):
+        print('TODO')
+
+    def test_lodge_open_lodge(self):
+        print('TODO')
+
+    def test_lodge_close_lodge(self):
+        print('TODO')
+
+    def test_lodge_open_food(self):
+        print('TODO')
+
+    def test_lodge_close_food(self):
+        print('TODO')
+
 if __name__ == "__main__":
     unittest.main()

1ed8289fa74db0944381b6e930e3055e290211a9
Luke Beatty <lcharlesb@Lukes-MacBook-Pro-3.local>
2020-02-25 20:00:05 -0500

    Validation for previously added functions.

--- mountain.py
+++ mountain.py
@@ -25,8 +25,10 @@ class Mountain:
         print(*self.lodges, sep = "\n")
 
     def trail_status(self, trail_name):
+        trail_found = False
         for trail in self.trails:
             if trail.get_name == trail_name:
+                trail_found = True
                 groomed = ""
                 open = ""
                 if trial.get_groomed == True:
@@ -38,21 +40,29 @@ class Mountain:
                 else:
                     open = "closed"
                 print(trail_name + " is currently " + groomed + " and " + open + ".")
+        if trail_found == False:
+            print("No trail by the name of " + trail_name + " exists on this mountain.")
 
 
     def lift_status(self, lift_name):
+        lift_found = False
         for lift in self.lifts:
             if lift.get_name == lift_name:
+                lift_found = True
                 running = ""
                 if lift.get_running == True:
                     running = "running"
                 else:
                     running = "not running"
                 print(lift_name + " is currently " + running + ".")
+        if lift_found == False:
+            print("No lift by the name of " + lift_name + " exists on this mountain.")
 
     def lodge_status(self, lodge_name):
+        lodge_found = False
         for lodge in self.lodges:
             if lodge_name == lodge.get_name:
+                lodge_found = True
                 open = ""
                 food = ""
                 if lodge.get_open == True:
@@ -64,6 +74,8 @@ class Mountain:
                 else:
                     food = "not serving food"
                 print(lodge_name + " is currently " + open + " and " + food + ".")
+        if lodge_found == False:
+            print("No lodge by the name of " + lodge_name + " exists on this mountain.")
 
     def add_trail(self, trail):
         self.trails.append(trail)

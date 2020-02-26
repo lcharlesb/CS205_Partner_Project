cb6225f1c32997319c1331f18655b104c8aec4af
Luke Beatty <lcharlesb@Lukes-MacBook-Pro-3.local>
2020-02-25 20:40:30 -0500

    Added comments.

--- unittests.py
+++ unittests.py
@@ -9,16 +9,20 @@ class TestMountain(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        # called one time, at beginning
         print('setUpClass()')
 
     @classmethod
     def tearDownClass(cls):
+        # called one time, at end
         print('tearDownClass()')
 
     def setUp(self):
+        # called before every test
         print('setUp()')
 
     def tearDown(self):
+        # called after every test
         print('tearDown()')
 
 if __name__ == "__main__":
